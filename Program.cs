ee98ccd84d1cf60999f5d35669d63d16436b5a1d
Lou Campopiano <campop74@gmail.com>
2020-03-09 10:09:32 -0400

    parameterized

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using VideoDownloader.con.Downloader;
+﻿using System;
+using VideoDownloader.con.Downloader;
 
 namespace VideoDownloader.con
 {
@@ -6,7 +7,14 @@ namespace VideoDownloader.con
     {
         static void Main(string[] args)
         {
-            var url = "https://www.youtube.com/embed/wzhcZ-aBgBg.mp4";
+            if (args.Length != 1)
+            {
+                Console.WriteLine("Usage VideoDownloader.con.exe <videoId>");
+                Console.Read();
+                return;
+            }
+            var videoId = args[0];
+            var url = $"https://www.youtube.com/embed/{videoId}.mp4";
             var vds = new VideoDownloaderService();
             vds.Download(url);
         }
