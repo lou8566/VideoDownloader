97de011631170b5a3146be91f943c82e8d5a34b4
Lou Campopiano <campop74@gmail.com>
2020-03-09 09:34:27 -0400

    first commit

---
+++ Downloader/VideoDownloaderService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using VideoLibrary;
+
+namespace VideoDownloader.con.Downloader
+{
+    public class VideoDownloaderService
+    {
+        public void Download(string url)
+        {
+            var VedioUrl = url;
+            var youTube = YouTube.Default;
+            var video = youTube.GetVideo(VedioUrl);
+            var path = "/youtube/" + video.FullName;
+            Console.WriteLine("Downloading...");
+            System.IO.File.WriteAllBytes(path, video.GetBytes());
+            Console.WriteLine("Completed");
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using VideoDownloader.con.Downloader;
+
+namespace VideoDownloader.con
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var url = "https://www.youtube.com/embed/wzhcZ-aBgBg.mp4";
+            var vds = new VideoDownloaderService();
+            vds.Download(url);
+        }
+    }
+}
